8409c980c8ff090447b1b012959c9fb3a864c6b8
Tossapon Nuanchuay <ntossapo@gmail.com>
2021-07-18 22:19:20 +0700

    bind 0.0.0.0

--- serverside/main.py
+++ serverside/main.py
@@ -134,4 +134,4 @@ def resetdf():
     return json.dumps([int(P7), int(P14), int(P21), int(P28)])
 
 if __name__ == '__main__': 
-    app.run(host='127.0.0.1',port=8000,debug=False)
+    app.run(host='0.0.0.0',port=8000,debug=False)
